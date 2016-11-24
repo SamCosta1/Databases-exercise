5726801a3194c45e75ee55cfc1b7baaa26f0fd00
Sam <sam.dacosta@student.manchester.ac.uk>
2016-11-24 20:34:16 +0000

    Finish ex3

---
+++ ex3/buildScript.sql
@@ -0,0 +1,7 @@
+START /opt/info/courses/COMP23111/drop-Accident-tables.sql;
+START /opt/info/courses/COMP23111/create-Accident-tables.sql;
+START /opt/info/courses/COMP23111/populate-Accident-tables.sql;
+
+START /opt/info/courses/COMP23111/drop-University-tables.sql;
+START /opt/info/courses/COMP23111/create-University-tables.sql;
+START /opt/info/courses/COMP23111/populate-University-tables.sql;
